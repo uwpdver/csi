9412abd765cbf60db2c88e17241129128decd22a
Zhihao Liu <1119548217@qq.com>
2022-09-18 14:56:03 +0800

    refactor: 整理后端代码结构

--- pages/api/room/services.ts
+++ pages/api/room/services.ts
@@ -1,25 +1,47 @@
 import { prisma } from "@/lib/prisma";
 
-// 用户进入房间
-export async function enterRoom(userId: number, roomId: number) {
-  const room = await prisma.room.findUnique({
-    where: {
-      id: roomId,
-    },
-    include: {
-      users: true,
+// 创建新游戏房间
+export async function createRoom(userId: number, title: string) {
+  const room = await prisma.room.create({
+    data: {
+      hostId: userId,
+      title,
     },
   });
+  return room;
+}
 
+// 用户进入房间
+export async function enterRoom(userId: number, roomId: number) {
+  const room = await getDetailById(roomId);
   if (!room) {
-    throw new Error("room doest not exsit");
+    throw new Error("房间不存在");
   }
-
   if (room.users.find((user) => userId === user.userId)) {
-    throw new Error("you are aready in the room");
+    throw new Error("该用户已在房间中");
   }
+  return addUserToRoom(userId, roomId);
+}
 
-  const result = await prisma.room.update({
+// 用户离开房间
+export async function leaveRoom(userId: number, roomId: number) {
+  try {
+    const room = await removeUserFromRoom(userId, roomId);
+    if (room.users.length === 0) {
+      return deleteRoom(roomId);
+    } else if (room.hostId === userId) {
+      return changeRoomHost(room.users[0].userId, roomId);
+    } else {
+      return room;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// 将一名用户从房间中移除
+export async function addUserToRoom(userId: number, roomId: number) {
+  return await prisma.room.update({
     where: {
       id: roomId,
     },
@@ -41,72 +63,64 @@ export async function enterRoom(userId: number, roomId: number) {
       },
     },
   });
-  return result;
 }
 
-// 用户离开房间
-export async function leaveRoom(userId: number, roomId: number) {
-  const roomInclude = {
-    users: {
-      include: {
-        user: true,
+// 将一名用户从房间中移除
+export async function removeUserFromRoom(userId: number, roomId: number) {
+  return await prisma.room.update({
+    where: {
+      id: roomId,
+    },
+    data: {
+      users: {
+        deleteMany: {
+          userId,
+        },
       },
     },
-  };
-
-  try {
-    const room = await prisma.room.update({
-      where: {
-        id: roomId,
-      },
-      data: {
-        users: {
-          deleteMany: {
-            userId,
-          },
+    include: {
+      users: {
+        include: {
+          user: true,
         },
       },
-      include: roomInclude,
-    });
-    if (room.users.length === 0) {
-      return await prisma.room.delete({
-        where: {
-          id: roomId,
-        },
-        include: roomInclude,
-      });
-    } else if (room.hostId === userId) {
-      return await prisma.room.update({
-        where: {
-          id: roomId,
-        },
-        data: {
-          host: {
-            connect: {
-              id: room.users[0].userId,
-            },
-          },
-        },
-        include: roomInclude,
-      });
-    } else {
-      return room;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-export async function createRoom(userId: number, title: string) {
-  const room = await prisma.room.create({
-    data: {
-      hostId: userId,
-      title,
     },
   });
-  return room;
 }
 
+// 删除游戏房间
+export async function deleteRoom(roomId: number) {
+  return await prisma.room.delete({
+    where: {
+      id: roomId,
+    },
+  });
+}
+
+// 改变房间的房主
+export async function changeRoomHost(userId: number, roomId: number) {
+  return await prisma.room.update({
+    where: {
+      id: roomId,
+    },
+    data: {
+      host: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+    include: {
+      users: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
+}
+
+// 获取房间详情
 export async function getDetailById(id: number) {
   const room = await prisma.room.findUnique({
     where: {
@@ -124,6 +138,7 @@ export async function getDetailById(id: number) {
   return room;
 }
 
+// 获取用户所在的房间
 export async function getUserRoom(userId: number) {
   const userInRoom = await prisma.userInRoom.findUnique({
     where: {
@@ -136,6 +151,7 @@ export async function getUserRoom(userId: number) {
   return userInRoom?.room;
 }
 
+// 修改用户准备状态
 export async function changeUserReadyState(userId: number, isReady: boolean) {
   const result = await prisma.userInRoom.update({
     where: {
